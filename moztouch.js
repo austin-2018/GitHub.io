278f43f0798388976ff7f7b629c7d8116b2b2d27
austin-2018 <38106983+austin-2018@users.noreply.github.com>
2018-04-09 16:54:25 -0400

    Delete moztouch.js

--- moztouch.js
+++
@@ -1,9 +0,0 @@
-//https://developer.mozilla.org/en-US/docs/Web/API/Touch_events
-function startup() {
-  var el = document.getElementsByTagName("canvas")[0];
-  el.addEventListener("touchstart", handleStart, false);
-  el.addEventListener("touchend", handleEnd, false);
-  el.addEventListener("touchcancel", handleCancel, false);
-  el.addEventListener("touchmove", handleMove, false);
-  log("initialized.");
-}
